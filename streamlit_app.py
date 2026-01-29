c8c7d9c93e5668c1fcacd4a1bc15ec441fa56e08
Rabii Mahnouk <rmahnouk@gmail.com>
2026-01-29 15:10:13 +0100

    Update streamlit_app.py
    update code to fix issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 #st.title(f" :cup_with_straw: Customize Your Smoothie! :cup_with_straw: ")
@@ -57,7 +58,7 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' +name_on_order +'!' , icon="✅")
 
 
-import requests
+
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=true)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
