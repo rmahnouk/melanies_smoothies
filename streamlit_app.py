2107311ddc59d1baf704e0f014ece0091b1b9542
Rabii Mahnouk <rmahnouk@gmail.com>
2026-01-29 16:25:30 +0100

    Update streamlit_app.py
    Add the New SEARCH_ON Column to the Dataframe that feeds the Multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,8 @@ st.write(
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("fruit_name"))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 name_on_order = st.text_input("Name on Smoothie")
 #st.write("The current movie title is", title)
